8779bacf13e2ea27785b0d6d10f067c4d1abecdb
Carlos Rivas <101880304+CarlosRivasG@users.noreply.github.com>
2023-08-11 02:41:44 -0400

    navbar y footer completos ahora vamos con django

---
+++ src/containers/Errors/Error404.jsx
@@ -0,0 +1,15 @@
+import Layout from "../../hocs/Layout" 
+
+
+
+const Error404 = () => {
+    return (
+        <Layout className="text-blue-600">           
+               Error 404  
+
+                             
+        </Layout>
+    );
+};
+
+export default Error404;
---
+++ src/containers/Home.jsx
@@ -0,0 +1,16 @@
+import Layout from "../hocs/Layout";
+
+
+const Home = () => {
+    return (
+        <Layout >      
+            <div className="text-3xl font-bold underline"> 
+                home dd
+
+            </div>     
+
+        </Layout>
+    );
+};
+
+export default Home;
